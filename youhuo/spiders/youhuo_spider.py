0424cc382ac48e695aedf97cbc16c2a3b107b3b6
edison <2499090390@qq.com>
2017-09-10 23:51:12 +0800

    ttt

--- youhuo/spiders/youhuo_spider.py
+++ youhuo/spiders/youhuo_spider.py
@@ -13,23 +13,24 @@ class YouHuoSpider(Spider):
     def parse(self, response):
         text=response.body.decode("utf-8")
         sel=Selector(response)
+        #
+        # name=sel.xpath('//div[@class="pull-right infos"]/h1[@class="name"]/text()').extract()
+        # if name:
+        #     name=print(name[0])
+        # origin_price=sel.xpath('//span[@class="price has-other-price"]/text()').extract()
+        # if origin_price:
+        #     origin_price=origin_price[0].decode("utf-8")
+        # current_price=sel.xpath('//span[@class="promotion-price"]/span[@class="price"]/text()').extract()
+        # if not current_price:
+        #     current_price=sel.xpath('//span[@class="price-row"]/span[@class="price"]/text()').extract()
 
-        name=sel.xpath('//div[@class="pull-right infos"]/h1[@class="name"]/text()').extract()
-        if name:
-            name=print(name[0])
-        origin_price=sel.xpath('//span[@class="price has-other-price"]/text()').extract()
-        if origin_price:
-            origin_price=origin_price[0].decode("utf-8")
-        current_price=sel.xpath('//span[@class="promotion-price"]/span[@class="price"]/text()').extract()
-        if not current_price:
-            current_price=sel.xpath('//span[@class="price-row"]/span[@class="price"]/text()').extract()
-
-        if current_price:
-            current_price=str(current_price[0])
+        # if current_price:
+        #     current_price=str(current_price[0])
         price_info=re.findall('PING_YOU_VIEW_ITEM =(.*?);',response.body.decode("utf-8"),re.S)
-        print(price_info[0])
         if price_info:
             price_info=json.loads(price_info[0].replace('// 宽x高','').replace("'",'"'))
+            origin_price=price_info['orig_price']
+            current_price=price_info['price']
         print(current_price)
         print(origin_price)
         print(price_info)
